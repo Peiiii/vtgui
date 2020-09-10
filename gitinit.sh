e4d014bacc717d27064dd6f6ad2a3d070d796dc9
Peiiii <1535376447@qq.com>
2020-09-10 16:51:45 +0800

    first commit

--- gitinit.sh
+++ gitinit.sh
@@ -1,5 +1,5 @@
 git init
 git add .
 git commit -m "first commit"
-git remote add origin git@github.com:Peiiii/vtgui
+git remote add origin git@github.com:Peiiii/vtgui.git
 git push -u origin master
